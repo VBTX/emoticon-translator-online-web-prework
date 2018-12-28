a25f42399f398a2fa6a28face41a58c3b18f649c
Victoria B <viktorija.bourassa@gmail.com>
2018-12-28 17:04:38 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -2,7 +2,12 @@ require "yaml"
 emoticons = YAML.load_file('lib/emoticons.yml')
 
 def load_library(emoticons)
-  hash = {:get_meaning => {}, :get_emoticon => {}}
+  hash[:get_meaning] = {}
+  hash [:get_emoticon] => {}
+  emoticons.each do |k, v|
+    hash[:get_meaning][v[1]] = k
+    hash [:get_emoticon][v[0]] = hash[:get_meaning][v[1]]
+  end
 end
 
 def get_japanese_emoticon(emoticons, emoticon)
