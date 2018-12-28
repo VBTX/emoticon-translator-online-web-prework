2cded38d072bf238f2a95ae3320e90c1df656b72
Victoria B <viktorija.bourassa@gmail.com>
2018-12-28 17:54:48 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -16,7 +16,7 @@ def get_japanese_emoticon(emoticons, english_emoticon)
   hash = load_library(emoticons)
   emoticons = YAML.load_file('./lib/emoticons.yml')
   if hash["get_emoticon"].include?(english_emoticon)
-     hash["get_emoticon"]
+     hash["get_emoticon"][english_emoticon]
      else
       return "Sorry, that emoticon was not found"
   end
