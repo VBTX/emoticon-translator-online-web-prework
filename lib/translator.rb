958f0eb8a33b7b6f5b4c1a0c40a2c0e11e8fd1a4
Victoria B <viktorija.bourassa@gmail.com>
2018-12-28 17:15:10 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -3,8 +3,8 @@ emoticons = YAML.load_file('lib/emoticons.yml')
 
 def load_library(emoticons)
   hash = Hash.new
-  hash[:get_meaning] = {}
-  hash[:get_emoticon] = {}
+  hash[:get_meaning]
+  hash[:get_emoticon] 
   return hash
 end
 
