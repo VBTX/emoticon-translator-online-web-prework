784a3fc72ed9ef6774230270b576ee851b3330b7
Victoria B <viktorija.bourassa@gmail.com>
2018-12-28 17:04:42 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -8,6 +8,7 @@ def load_library(emoticons)
     hash[:get_meaning][v[1]] = k
     hash [:get_emoticon][v[0]] = hash[:get_meaning][v[1]]
   end
+  hash
 end
 
 def get_japanese_emoticon(emoticons, emoticon)
