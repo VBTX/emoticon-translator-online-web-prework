c1591da87a6b30605caf52247d3ad8beca7d8198
Victoria B <viktorija.bourassa@gmail.com>
2018-12-28 17:11:08 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -3,6 +3,9 @@ emoticons = YAML.load_file('lib/emoticons.yml')
 
 def load_library(emoticons)
   hash = Hash.new
+  hash[:get_meaning] = {}
+  hash[:get_emoticon] = {}
+  return hash
 end
 
 def get_japanese_emoticon(emoticons, emoticon)
